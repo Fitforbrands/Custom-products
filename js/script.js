ae71c93d7ab6412e4794f2a94c7580014a4c9b36
gabrielcapuia <gabriel@fitforbrands.com>
2024-11-27 22:19:53 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const appOptions = {
+  stageWidth: 600,
+  stageHeight: 900,
+  fileServerURL: "../img/book-prowork.png",
+};
+
+const fpd = new FancyProductDesigner(
+  document.getElementById("fpd-target"),
+  appOptions
+);
